e643be85e6a0c4ebda0c1e43ced42992c0f8b4ae
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-15 09:54:57 -0800

    .....

--- task1a.py
+++ task1a.py
@@ -1,4 +1,5 @@
 # to complete task1 a of open book
+import string
 
 fout = open("Book1.txt",'r')
 fout = fout.read()
@@ -12,5 +13,19 @@ def unique_words():
     else:
       unique_word[word] += 1
 
+def count_the_article():
+  str1 = []
+  fout = open("Book1.txt",'r')
+  r = fout.readlines()
+  for line in r:
+    str2 = line.lower().strip(string.punctuation + string.whitespace)
+    str1 += str2
+  total = {}
+  for word in  str1:
+    total[word] = total.get(word, 0) + 1
+  words_t = len(str1)
+  words_d = len(total.keys())
+  print("Total Words : " + str(words_d))
 
-unique_words()
+print(count_the_article())
+#unique_words()
