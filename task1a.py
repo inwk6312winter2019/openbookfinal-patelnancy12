a3e2bde52af17e907f1556309d71e80acd392145
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-15 09:49:10 -0800

    ...

--- task1a.py
+++ task1a.py
@@ -1,1 +1,16 @@
 # to complete task1 a of open book
+
+fout = open("Book1.txt",'r')
+fout = fout.read()
+words = fout.split("\n")
+
+def unique_words():
+  unique_words = []
+  if words in fout:
+    if words not in fout:
+      unique_words[word] = 1
+    else:
+      unique_word[word] += 1
+
+
+unique_words()
