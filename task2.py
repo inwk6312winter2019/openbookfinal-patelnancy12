1b76e66fb2eb3ac163d1db55e31178524ba7977b
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-15 11:10:54 -0800

    .....

--- task2.py
+++ task2.py
@@ -1,8 +1,18 @@
 # to complete task 2 of open book
+fopen = open("access.log","r")
+fget = open("get.log","w")
+fpost = open("post.log","w")
+fput = open("put.log","w")
+fdelete = open("delete.log","w")
+for fpe in fopen:
+  if "GET /" in fpe:
+    fget.write(fpe)
+  elif "POST /" in fpe:
+    fpost.write(fpe)
+  elif "PUT /" in fpe:
+    fput.write(fpe)
+  else:
+    fdelete.write(fpe)
 
-fout = open ("get.log", "w")
-fout = open("put.log", "w")
-fout = open("post.log", "w"
-fout = open("delete.log" , "w")
 
 
