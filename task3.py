71dfdbe8b7c16edfe646f9986e25d86d33fc14a4
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-15 11:18:43 -0800

    .....

--- task3.py
+++ task3.py
@@ -1,2 +1,14 @@
-# to complete task 3 of open book
+import requests
+import json
+def geticket():
+    url = "https://sandboxapicem.cisco.com/api/v1/ticket"
+    data = {}
+    data["username"] = "devnetuser"
+    data["password"] = "Cisco123!"
+    data = json.dumps(data)
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=data,headers=headers)
+    print(response.json()['response']['serviceTicket'])
+
+geticket()# to complete task 3 of open book
 
