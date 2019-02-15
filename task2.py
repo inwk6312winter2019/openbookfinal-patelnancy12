748205d9673bcdf1a485ec8fcdf23ddc3e3a8c9e
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-15 11:16:14 -0800

    .....

--- task2.py
+++ task2.py
@@ -14,5 +14,19 @@ for fpe in fopen:
   else:
     fdelete.write(fpe)
 
+def topip():m
+  mydict = dict()
+  fopen = open("access.log","r")
+    for fop in fopen:
+      if "GET /" in fop or "POST /" in fop:
+        ip = fop.split('-')[0]
+          if ip not in mydict:
+            mydict[ip] = 1
+          else:
+            mydict[ip] += 1
+    sorted_x = sorted(mydict, key=mydict.get, reverse=True)[:20]
+    return sorted_x
 
+
+print(topip())
 
