7cf3c6900bdcbf3c612ccc8b785b23680243c75b
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-15 10:54:24 -0800

    .....

--- task1b.py
+++ task1b.py
@@ -1,19 +1,23 @@
 # to complete task 1b of open book final exam
 import string
-import colletions
-import sys
  
 def frequent_words():
   str1 = []
+
   fout = open("Book1.txt",'r')
   fout = open("Book2.txt", 'r')
   fout = open("Book3.txt", 'r')
-
-  r = fout.read().lower()
-  words = collections.counter(r.split())
-  print(words)
-
-
+  r = fout.readlines()
+  mydict = dict()
+  for line in r:
+    str2 = line.lower().strip(string.punctuation + string.whitespace)
+    str1 += str2
+    for item in str1:
+      if item not in mydict:
+        mydict[item] = 1
+      else:
+        mydict[item] += 1
+ 
 
 print(frequent_words())
 
