303363f1e2da71747ffacec41d975371ea434e5a
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-15 10:42:27 -0800

    ....

--- task1b.py
+++ task1b.py
@@ -1,1 +1,19 @@
 # to complete task 1b of open book final exam
+import string
+import colletions
+import sys
+ 
+def frequent_words():
+  str1 = []
+  fout = open("Book1.txt",'r')
+  fout = open("Book2.txt", 'r')
+  fout = open("Book3.txt", 'r')
+
+  r = fout.read().lower()
+  words = collections.counter(r.split())
+  print(words)
+
+
+
+print(frequent_words())
+
